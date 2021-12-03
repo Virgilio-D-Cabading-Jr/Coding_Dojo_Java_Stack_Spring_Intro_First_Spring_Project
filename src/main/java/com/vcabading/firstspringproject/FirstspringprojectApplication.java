f11e1ee4e9d555667251c5e3893d0cb6f0c3390c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-02 20:44:43 -0800

    Add helo world

--- src/main/java/com/vcabading/firstspringproject/FirstspringprojectApplication.java
+++ src/main/java/com/vcabading/firstspringproject/FirstspringprojectApplication.java
@@ -3,11 +3,20 @@ package com.vcabading.firstspringproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class FirstspringprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstspringprojectApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello Spring World!";
+	}
+	
 }
