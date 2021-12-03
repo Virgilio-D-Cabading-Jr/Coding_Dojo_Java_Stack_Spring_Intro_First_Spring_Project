1dd114b5198a72e1c5528d58c550fb53b5038f83
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-02 19:49:21 -0800

    initial commit

---
+++ src/test/java/com/vcabading/firstspringproject/FirstspringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcabading.firstspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
