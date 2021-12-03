2c303cc174620f8ffa124e4b0c1b1aeaa1108b80
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-02 21:36:14 -0800

    add class mapping

--- src/main/java/com/vcabading/firstspringproject/HomeController.java
+++ src/main/java/com/vcabading/firstspringproject/HomeController.java
@@ -4,8 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HomeController {
-	@RequestMapping("/")
+	@RequestMapping("")
 	public String hello() {
 		return "Hello Spring World!";
 	}
